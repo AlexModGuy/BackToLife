608a7bbe9b52f7382e9b43c9fe3a4aa236d246dd
Alex-the-666 <alex.rowlands@cox.net>
2016-05-25 20:05:28 -0700

    Fixed #1, but new issues have arised

--- src/main/java/com/github/backtolifemod/backtolife/item/ItemFossil.java
+++ src/main/java/com/github/backtolifemod/backtolife/item/ItemFossil.java
@@ -25,15 +25,15 @@ public class ItemFossil extends Item {
 		GameRegistry.registerItem(this, "unknown_fossil_" + name);
 	}
 
-    @SideOnly(Side.CLIENT)
-    public void addInformation(ItemStack stack, EntityPlayer playerIn, List<String> tooltip, boolean advanced){
-    	tooltip.add(I18n.translateToLocal("item.backtolife.unknown_fossil_" + fossil + ".desc"));
-    }
-    
+	@SideOnly(Side.CLIENT)
+	public void addInformation(ItemStack stack, EntityPlayer playerIn, List<String> tooltip, boolean advanced){
+		tooltip.add(I18n.translateToLocal("item.backtolife.unknown_fossil_" + fossil + ".desc"));
+	}
+
 	@SideOnly(Side.CLIENT)
 	public void getSubItems(Item itemIn, CreativeTabs tab, List<ItemStack> subItems){
-			subItems.add(new ItemStack(itemIn, 1, 0));
-			subItems.add(new ItemStack(itemIn, 1, 1));
-			subItems.add(new ItemStack(itemIn, 1, 2));
+		subItems.add(new ItemStack(itemIn, 1, 0));
+		subItems.add(new ItemStack(itemIn, 1, 1));
+		subItems.add(new ItemStack(itemIn, 1, 2));
 	}
 }
